a2517048ec1b59e62fc27e47d68812ab8735db5f
Shahbaz Ahmad <msa413@gmail.com>
2018-10-16 15:18:18 +0500

    Add message service

--- server/server.js
+++ server/server.js
@@ -5,14 +5,15 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-  io.on('connection', (socket) => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
 
 
 
@@ -22,16 +23,14 @@ app.use(express.static(publicPath));
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
   });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
-
-  console.log('New user connected');
 });
 
 server.listen(port, () => {
-  console.log(`Server is app on ${port}`);
+  console.log(`Server is up on ${port}`);
 });
