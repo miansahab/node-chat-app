a62521f15ccba3946e7bd7bfcb4f75de20fb5c62
Shahbaz Ahmad <msa413@gmail.com>
2018-10-19 11:42:29 +0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 //var moment = require('moment');
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -18,6 +35,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // var formattedTime = moment(message.createdAt).format('h:mm a');
   // console.log('newMessage', message);
@@ -38,6 +56,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   // // alert(message.url);
   // var li = jQuery('<li></li>');
   // //consol.url('map url', message.url);
