be8b8f3d499595ea507133c05fb70fcdb40d8408
Shahbaz Ahmad <msa413@gmail.com>
2018-10-19 11:12:17 +0500

    Add mustache.js for messages template

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,42 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}  ${formattedTime}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // console.log('newMessage', message);
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from}  ${formattedTime}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  // alert(message.url);
-  var li = jQuery('<li></li>');
-  //consol.url('map url', message.url);
-  var a = jQuery('<a target="_blank">My current Location</a>')
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href',`${message.url}`);
-  li.append(a);
-  jQuery('#messages').append(li);
+
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  // // alert(message.url);
+  // var li = jQuery('<li></li>');
+  // //consol.url('map url', message.url);
+  // var a = jQuery('<a target="_blank">My current Location</a>')
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href',`${message.url}`);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
